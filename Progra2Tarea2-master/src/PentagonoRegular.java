db1588c9b6e54c3f59f61dac6494290c03eefead
Kevin Mendez <gerardo_soft@unitec.edu>
2015-03-28 16:55:56 -0600

    Update PentagonoRegular.java

--- Progra2Tarea2-master/src/PentagonoRegular.java
+++ Progra2Tarea2-master/src/PentagonoRegular.java
@@ -1,22 +1,22 @@
 public class PentagonoRegular {
 	int lado;
 
-	PentagonoRegular(int lado)
+	PentagonoRegular(int lado) // esta clase envia el lado del pentagono
 	{
 		this.lado = lado;
 	}
 	
-	int getLado()
+	int getLado() // este clase envia el lado del pentagono
 	{
 		return lado;
 	}
 	
-	void setLado(int lado)
+	void setLado(int lado) // este envia el lado del pentago
 	{
 		this.lado = lado;
 	}
 	
-	int getPerimetro()
+	int getPerimetro() // este toma el perimetro al pentagono
 	{
 		return lado*5;
 	}
