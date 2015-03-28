77873cae39eeddfc7098a0541de5f5b26f9f638f
Kevin Mendez <gerardo_soft@unitec.edu>
2015-03-28 16:57:45 -0600

    Update Rectangulo.java

--- Progra2Tarea2-master/src/Rectangulo.java
+++ Progra2Tarea2-master/src/Rectangulo.java
@@ -1,35 +1,35 @@
 public class Rectangulo {
 int altura, base;
 	
-	Rectangulo(int base, int altura)
+	Rectangulo(int base, int altura) // este esl e constructor del rectangulo
 	{
 		this.base = base;
 		this.altura = altura;		
 	}
 	
-	int getAltura()
+	int getAltura() // esta clase envia la altura
 	{
 		return altura;
 	}
-	int getBase()
+	int getBase() //esta clase envia la base
 	{
 		return base;
 	}
 	
-	void setAltura(int altura)
+	void setAltura(int altura) // esta clase modifica la altura
 	{
 		this.altura = altura;
 	}
-	void setBase(int base)
+	void setBase(int base) // este clase modigica la base
 	{
 		this.base = base;
 	}
 	
-	int getPerimetro()
+	int getPerimetro() // esta clase modifica el perimetro
 	{
 		return (2*(altura+base));
 	}
-	int getArea()
+	int getArea() // este modigica el area
 	{
 		return (altura*base);
 	}
